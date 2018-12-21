a6facbaf8f20ef88f8a590751b36ff73f0168d45
Ivan_Fan <Ivan_Fan@epam.com>
2018-12-21 14:39:03 +0800

    add express

---
+++ javascript/advance/React/React.16/new-feature/src/6.refs/App.js
@@ -0,0 +1,16 @@
+/**
+ * https://juejin.im/post/5be409696fb9a049b13db042
+ */
+import React from 'react'
+import Demo1 from './demo.1'
+ 
+class App extends React.Component {
+    render () {
+        return (
+            <>
+                <Demo1 />
+            </>
+        )
+    }
+}
+export default App
---
+++ javascript/advance/React/React.16/new-feature/src/6.refs/demo.1.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const TextInput = React.forwardRef((props, ref) => (
+    <input type="text" placeholder="Hello forwardRef" ref={ref} />
+))
+
+const inputRef = React.createRef()
+
+class Demo1 extends React.Component {
+    constructor(props) {
+        super(props)
+        this.myRef = React.createRef()
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+
+        alert('input value is:' + inputRef.current.value)
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <TextInput ref={inputRef} />
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
+}
+
+export default Demo1
