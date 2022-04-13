74a41709b6e9608255cf3b4cd6ef0e6e57b42337
594774261@qq.com <594774261@qq.com>
2022-04-14 00:06:49 +0800

    feat: update

---
+++ javascript/base/算法/27.双指针/142. 环形链表 II.js
@@ -0,0 +1,11 @@
+var detectCycle = function(head) {
+    const visited = new Set();
+    while (head !== null) {//终止条件，如果没有环 跳出循环
+        if (visited.has(head)) {//如果存在重复的节点，这个节点就是入环节点
+            return head; // 返回指定的节点
+        }
+        visited.add(head);//将节点加入set中
+        head = head.next;
+    }
+    return null;
+};
