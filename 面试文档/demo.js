feadd6178430cd81edabe374cd67be14cff0b0c9
594774261@qq.com <594774261@qq.com>
2022-05-17 00:48:54 +0800

    feat:update

--- 面试文档/demo.js
+++ 面试文档/demo.js
@@ -1,13 +1,18 @@
-Number.prototype.add = function (number) {
-  if (typeof number !== 'number') {
-      throw new Error('请输入数字～');
+const nums = [0, 1, 5, 3, 0, 3, 12, 0, 0, 8]
+const func = function (nums) {
+  let left = 0, right = nums.length - 1;
+  let firstZone = -1;
+  while (left <= right) {
+    if (nums[left] !== 0) {
+      nums[firstZone] = nums[left]
+      nums[left] = 0;
+      firstZone = firstZone + 1
+      left = left + 1;
+    } else {
+      firstZone = firstZone > -1 ? firstZone : left
+      left++
+    }
   }
-  return this + number;
-};
-Number.prototype.minus = function (number) {
-  if (typeof number !== 'number') {
-      throw new Error('请输入数字～');
-  }
-  return this - number;
-};
-console.log((5).add(3).minus(2));
+  return nums
+}
+console.log(func(nums))
