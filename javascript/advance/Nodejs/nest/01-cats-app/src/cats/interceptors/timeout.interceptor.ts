fca06a620829758642a431806e9bf392852872b7
hymn-github <840019534@qq.com>
2022-09-05 15:15:42 +0800

    update

--- javascript/advance/Nodejs/nest/01-cats-app/src/cats/interceptors/timeout.interceptor.ts
+++ javascript/advance/Nodejs/nest/01-cats-app/src/cats/interceptors/timeout.interceptor.ts
@@ -1,7 +1,7 @@
-import { RequestTimeoutException } from "src/common";
+ import { RequestTimeoutException } from "../../common";
 import { Observable, timeout, catchError, TimeoutError, throwError } from "rxjs";
-import { Injectable } from "src/common/decorators";
-import { CallHandler, ExecutionContext, NestInterceptor } from "src/common/interfaces";
+import { Injectable } from "../../common/decorators";
+import { CallHandler, ExecutionContext, NestInterceptor } from "../../common/interfaces";
 // https://juejin.cn/post/7097941883778777125?share_token=59cbb38c-6056-4c86-8793-2b05392ce727
 @Injectable()
 export class TimeoutInterceptor implements NestInterceptor {
